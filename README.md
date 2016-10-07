dd43426df997d0abaca77768f3c5d8e43ef827b4
akky <akky@akky.jp>
2016-10-07 22:22:50 +0900

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # wareki
 show Japanese calendar centennials/semicentennials
+
+# how to run
+
+~~~
+$ composer install
+$ php index.php
+~~~
+
+anniversary year list for two decades around the current year is generated, formatted in markdown.
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use JpnForPhp\Converter\Converter;
+
+$dateTime = new DateTime("now");
+$thisYear = (integer)($dateTime->format("Y"));
+
+for ($i = $thisYear - 10 ; $i < $thisYear + 10 ; $i++) {
+    format($i);
+}
+
+function format($targetYear) {
+    echo '### ' . $targetYear . '年'
+         . '(' . Converter::toJapaneseYear($targetYear) . ')は' . "\n" . PHP_EOL;
+
+    foreach (Converter::$mapEras as $era) {
+        $difference = $targetYear - $era['year'];
+        if (($difference % 50) === 0) {
+            echo ' * ' . formatEraWithWikipediaLink($era['kanji']) . "\t" . $difference . '年' . PHP_EOL;
+        }
+    }
+
+    echo "\n";
+}
+
+function formatEraWithWikipediaLink($eraName) {
+    return '[' . $eraName . '](' . 'https://ja.wikipedia.org/wiki/' . $eraName . ')';
+}
